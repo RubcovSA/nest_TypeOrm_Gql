4183b73f067dd8c531d246b2b05137c13f7ff619
Sergei Rubcov <rubcov.sa@gmail.com>
2021-05-10 08:54:05 +0300

    first test 🎉

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
+import { AuthorEntity } from '../src/author/entities/author.entity'
 
 describe('App', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
@@ -15,31 +16,50 @@ describe('App', () => {
     await app.init()
   })
 
+  afterAll(async () => {
+    await app.close()
+  })
+
   it('should create author', async () => {
+    const author: AuthorEntity = {
+      firstName: 'Marat',
+      lastName: 'Kutlugundini',
+    }
     const result = await request(app.getHttpServer())
-      .post('/public/api/v1/graphql')
+      .post('/graphql')
       .send({
         query: createAuthorQuery,
         variables: {
-          author: {
-            firstName: 'Marat',
-            lastName: 'Kutlugundini',
-          },
+          author,
         },
       })
       .set('Accept', 'application/json')
-    // .set('Authorization', `Bearer ${userToken}`)
-    // .expect('Content-Type', /json/)
-    // .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(200)
 
-    console.log(result)
-    // .endAsync()
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .get('/')
-    // .expect(200)
-    // .expect('Hello World!')
+    author.id = result.body.data.createAuthor.id
+    expect(result.body.data.createAuthor).toEqual(author)
+  })
+
+  it('should create author', async () => {
+    const author: AuthorEntity = {
+      firstName: 'Marat',
+      lastName: 'Kutlugundini',
+    }
+    const result = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: createAuthorQuery,
+        variables: {
+          author,
+        },
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    author.id = result.body.data.createAuthor.id
+    expect(result.body.data.createAuthor).toEqual(author)
   })
 })
 
